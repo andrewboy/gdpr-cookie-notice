9d13e1cf89dfd76b4f8a5e1600b4eecdc269565e
Beck András <beck.andras@brightly.hu>
2018-09-26 17:35:58 +0200

    test

--- dist/gdpr-cookie-notice.js
+++ dist/gdpr-cookie-notice.js
@@ -515,7 +515,7 @@ var _class = function () {
   }, {
     key: '_getModalTemplate',
     value: function _getModalTemplate() {
-      return '<div class="gdpr-cookie-notice">\n    <p class="gdpr-cookie-notice-description">{{description}}</p>\n    <nav class="gdpr-cookie-notice-nav">\n      <a href="#" class="gdpr-cookie-notice-nav-item gdpr-cookie-notice-nav-item-settings">{{settings}}</a>\n      <a href="#" class="gdpr-cookie-notice-nav-item gdpr-cookie-notice-nav-item-accept gdpr-cookie-notice-nav-item-btn">{{accept}}</a>\n    </nav>\n    </div>';
+      return '<div class="gdpr-cookie-notice-modal">\n      <div class="gdpr-cookie-notice-modal-content">\n        <div class="gdpr-cookie-notice-modal-header">\n          <h2 class="gdpr-cookie-notice-modal-title">{{settings}}</h2>\n          <button type="button" class="gdpr-cookie-notice-modal-close"></button>\n        </div>\n        <ul class="gdpr-cookie-notice-modal-cookies"></ul>\n        <div class="gdpr-cookie-notice-modal-footer">\n          <a href="#" class="gdpr-cookie-notice-modal-footer-item gdpr-cookie-notice-modal-footer-item-statement">{{statement}}</a>\n          <a href="#" class="gdpr-cookie-notice-modal-footer-item gdpr-cookie-notice-modal-footer-item-save gdpr-cookie-notice-modal-footer-item-btn">\n              <span>{{save}}</span>\n          </a>\n        </div>\n      </div>\n    </div>';
     }
   }, {
     key: '_getCategoryTemplate',
--- src/GdprCookieModal.js
+++ src/GdprCookieModal.js
@@ -154,12 +154,20 @@ export default class {
   }
 
   _getModalTemplate () {
-    return `<div class="gdpr-cookie-notice">
-    <p class="gdpr-cookie-notice-description">{{description}}</p>
-    <nav class="gdpr-cookie-notice-nav">
-      <a href="#" class="gdpr-cookie-notice-nav-item gdpr-cookie-notice-nav-item-settings">{{settings}}</a>
-      <a href="#" class="gdpr-cookie-notice-nav-item gdpr-cookie-notice-nav-item-accept gdpr-cookie-notice-nav-item-btn">{{accept}}</a>
-    </nav>
+    return `<div class="gdpr-cookie-notice-modal">
+      <div class="gdpr-cookie-notice-modal-content">
+        <div class="gdpr-cookie-notice-modal-header">
+          <h2 class="gdpr-cookie-notice-modal-title">{{settings}}</h2>
+          <button type="button" class="gdpr-cookie-notice-modal-close"></button>
+        </div>
+        <ul class="gdpr-cookie-notice-modal-cookies"></ul>
+        <div class="gdpr-cookie-notice-modal-footer">
+          <a href="#" class="gdpr-cookie-notice-modal-footer-item gdpr-cookie-notice-modal-footer-item-statement">{{statement}}</a>
+          <a href="#" class="gdpr-cookie-notice-modal-footer-item gdpr-cookie-notice-modal-footer-item-save gdpr-cookie-notice-modal-footer-item-btn">
+              <span>{{save}}</span>
+          </a>
+        </div>
+      </div>
     </div>`
   }
 
